56ae030937d9f139c6243959ad3ea50d7e6a790d
Bibek Regmi <bibekregmi@Bibek-Regmi-MBP.fios-router.home>
2020-05-28 08:16:00 -0400

    Add routing and auto route to fh

---
+++ src/app/fh/fh-landing-page/fh-landing-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fh-landing-page',
+  templateUrl: './fh-landing-page.component.html',
+  styleUrls: ['./fh-landing-page.component.scss']
+})
+export class FhLandingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
